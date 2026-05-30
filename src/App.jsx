2bf747832d5c9bd65d233e36d9ebcc43259aba96
Diana Balconi <dalebv87@gmail.com>
2026-05-30 14:02:36 -0600

    Implement the first part of the hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
 import React from 'react'
 import gsap from 'gsap'
-import { ScrollTrigger, SplitText } from 'gsap/all';
+import { ScrollTrigger, SplitText } from 'gsap/all'
+import { useGSAP } from '@gsap/react'
+import Navbar from './components/Navbar'
+import Hero from './components/Hero'
+
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
 const App = () => {
   return (
-    <div className='flex-center h-[100vh]'>
-        <h1 className="text-3xl text-indigo-500">Wanderlux Travel Agency</h1>
-    </div>
+    <main>
+      <Navbar />
+      <Hero />
+      <div className="h-dvh bg-black" />
+    </main>
   )
 }
+
 export default App
